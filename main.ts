37a758c8f0a0727f41bedf0cf6fcabf51c48a612
Sean Burlington <sean@practicalweb.co.uk>
2022-03-25 09:59:18 +0000

    added heartbeat

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@ input.onButtonPressed(Button.A, function () {
     timer = randint(5, 15)
     basic.showIcon(IconNames.Yes)
     while (timer > 0) {
-        basic.pause(1000)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallDiamond)
         timer += -1
     }
-    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.No)
 })
 let timer = 0
 basic.showIcon(IconNames.No)
